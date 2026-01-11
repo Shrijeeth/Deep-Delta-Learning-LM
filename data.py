2e07a7f3468e0634dbab53057f097fc5d957d966
shrijeeth-s-ai <shrijeeth.s@silqfi.com>
2026-01-12 02:20:04 +0530

    Add subset parameter storage to CustomPretrainingDataModule initialization to fix missing attribute error
    - Store subset parameter as instance variable in CustomPretrainingDataModule.__init__() to ensure it's available for dataset loading

--- data.py
+++ data.py
@@ -126,6 +126,7 @@ class CustomPretrainingDataModule(L.LightningDataModule):
         super().__init__()
         self.model_name = model_name
         self.dataset = dataset
+        self.subset = subset
         self.batch_size = batch_size
         self.max_length = max_length
         self.text_column = text_column
